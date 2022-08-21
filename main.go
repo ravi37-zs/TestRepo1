c2e3d625f5fc8f1764809d8e0f92ea7503d7e705
ravi37-zs <ravi.yadav@zopsmart.com>
2022-08-21 12:13:29 +0530

    change done

--- main.go
+++ main.go
@@ -3,11 +3,6 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("in master branch")
-	fmt.Println("this is my ayush ")
-	fmt.Println("in master branch")
-	fmt.Println("this is my branch ")
-	fmt.Println("in master branch")
-	fmt.Println("this is my branch ")
+	fmt.Println("github conflict")
 
 }
